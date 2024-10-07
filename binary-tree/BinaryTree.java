b7ad5766a1a89dbabeae533e870ef70bc548315f
John Loyd Mulit <jlmulit68@gmail.com>
2024-10-07 11:45:27 +0800

    feat: implement 3 traversals

--- binary-tree/BinaryTree.java
+++ binary-tree/BinaryTree.java
@@ -1,12 +1,10 @@
-import java.util.ArrayList;
-
 public class BinaryTree {
     public static void main(String[] args) {
-        Node node1 = new Node(2);
-        Node node2 = new Node(3);
-        Node node3 = new Node(4);
-        Node node4 = new Node(5);
-        Node node5 = new Node(6);
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
 
         node1.left = node2;
         node1.right = node3;
@@ -14,55 +12,41 @@ public class BinaryTree {
         node2.left = node4;
         node2.right = node5;
 
-        // visitChild(node1);
-        //
-        // int total = computeSum(node1);
-        // System.out.println("total: " + total);
-
-        breadthFirstTraversal(node1);
+        preOrderTraversal(node1);
     }
 
-    static Node visitChild(Node node) {
-        System.out.println("node: " + node.data);
-        if (node.left != null) {
-            visitChild(node.left);
+    static void preOrderTraversal(Node root) {
+        if (root == null) {
+            return;
         }
 
-        if (node.right != null) {
-            visitChild(node.right);
-        }
-
-        return node;
+        visit(root);
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
     }
 
-    static int computeSum(Node node) {
-        if (node.left != null) {
-            node.data += computeSum(node.left);
+    static void inOrderTraversal(Node root) {
+        if (root == null) {
+            return;
         }
 
-        if (node.right != null) {
-            node.data += computeSum(node.right);
-        }
-
-        return node.data;
+        inOrderTraversal(root.left);
+        visit(root);
+        inOrderTraversal(root.right);
     }
 
-    static void breadthFirstTraversal(Node node) {
-        ArrayList<Node> to_visit = new ArrayList<Node>();
-        to_visit.add(node);
-
-        while (to_visit.size() > 0) {
-            Node curr = to_visit.getFirst();
-            System.out.println("curr node: " + curr.data);
-
-            if (curr.left != null)
-                to_visit.add(curr.left);
-
-            if (curr.right != null)
-                to_visit.add(curr.right);
-
-            to_visit.removeFirst();
+    static void postOrderTraversal(Node root) {
+        if (root == null) {
+            return;
         }
+
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        visit(root);
+    }
+
+    static void visit(Node node) {
+        System.out.println(node.data);
     }
 }
 
